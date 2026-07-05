f8ff704c5fbe5d92b4a3a3e5d893d27f4e82756e
josipzunic <jzunic123@gmail.com>
2026-07-05 15:52:13 +0200

    Give frontend access to backend

--- backend/AcademyTask.API/Program.cs
+++ backend/AcademyTask.API/Program.cs
@@ -9,30 +9,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 var secretKey = Environment.GetEnvironmentVariable("JWT_SECRET");
 if (secretKey == null)
-    throw new InvalidOperationException("Please include the JWT_SECRET environment variable");
+  throw new InvalidOperationException(
+      "Please include the JWT_SECRET environment variable");
 
 var issuer = builder.Configuration["Jwt:Issuer"];
 var audience = builder.Configuration["Jwt:Audience"];
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+builder.Services
+    .AddAuthentication(options => {
+      options.DefaultAuthenticateScheme =
+          JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
     })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = issuer,
-            ValidAudience = audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
-        };
+    .AddJwtBearer(options => {
+      options.TokenValidationParameters = new TokenValidationParameters {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = issuer,
+        ValidAudience = audience,
+        IssuerSigningKey =
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+      };
     });
-
+builder.Services.AddCors(options => {
+  options.AddPolicy("AllowFrontend", policy => {
+    policy.WithOrigins("http://localhost:5173")
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+  });
+});
 
 builder.Services.AddAuthorization();
 
@@ -42,18 +49,16 @@ builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/openapi/v1.json", "AcademyTask API v1");
-    });
-    
+if (app.Environment.IsDevelopment()) {
+  app.MapOpenApi();
+  app.UseSwaggerUI(options => {
+    options.SwaggerEndpoint("/openapi/v1.json", "AcademyTask API v1");
+  });
 }
 
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 app.Run();
